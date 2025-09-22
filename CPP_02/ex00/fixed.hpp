977d977efd706d357ad34da2f75681ffaa0f99e2
souel-bo <souel-bo@e2r8p4.1337.ma>
2025-09-22 03:28:09 +0100

    starting the 02 day

---
+++ CPP_02/ex00/fixed.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: souel-bo <souel-bo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/22 03:21:01 by souel-bo          #+#    #+#             */
+/*   Updated: 2025/09/22 03:25:53 by souel-bo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed
+{
+    private :
+        int fixed;
+        static const int bits;
+    public :
+
+};
+
+#endif
---
+++ CPP_02/ex00/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: souel-bo <souel-bo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/22 03:24:06 by souel-bo          #+#    #+#             */
+/*   Updated: 2025/09/22 03:24:36 by souel-bo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fixed.hpp"
+
+int main( void ) {
+    Fixed a;
+    Fixed b( a );
+    Fixed c;
+    c = b;
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+    return 0;
+}
