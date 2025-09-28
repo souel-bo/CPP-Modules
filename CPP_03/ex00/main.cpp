41aab6c74319b604b07df30420bd79deb75cac67
souel-bo <souel-bo@e2r8p4.1337.ma>
2025-09-28 05:30:49 +0100

    ADD : first exercice

---
+++ CPP_03/ex00/main.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: souel-bo <souel-bo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/28 04:56:19 by souel-bo          #+#    #+#             */
+/*   Updated: 2025/09/28 05:29:35 by souel-bo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+int main()
+{
+    ClapTrap clapppppyyyyy("LKHDMA");
+    ClapTrap tatata("raaaaa9");
+
+    clapppppyyyyy.attack("raaaaa9");
+    tatata.takeDamage(5);
+    tatata.beRepaired(10);
+}
