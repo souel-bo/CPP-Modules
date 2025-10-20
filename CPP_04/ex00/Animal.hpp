7fee8e0356c22d1f808e12b152e2a0ed41a1f36b
souel-bo <souel-bo@e2r8p4.1337.ma>
2025-10-20 17:37:31 +0100

    ADD : START OF THE FIRST EXERCICE

---
+++ CPP_04/ex00/Animal.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: souel-bo <souel-bo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/19 18:11:37 by souel-bo          #+#    #+#             */
+/*   Updated: 2025/10/20 17:11:40 by souel-bo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+#define ANIMAL_HPP
+
+#include <iostream>
+
+class Animal
+{
+	protected :
+		std::string type;
+	public :
+		Animal();
+		Animal(const Animal &other);
+		Animal& operator=(const Animal& other);
+		~Animal();
+} ;
+
+#endif
---
+++ CPP_04/ex00/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: souel-bo <souel-bo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/19 18:15:19 by souel-bo          #+#    #+#             */
+/*   Updated: 2025/10/19 18:15:59 by souel-bo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Animal.hpp"
+
+int main()
+{
+    const Animal* meta = new Animal();
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
+    std::cout << j->getType() << " " << std::endl;
+    std::cout << i->getType() << " " << std::endl;
+    i->makeSound();
+    j->makeSound();
+    meta->makeSound();
+    return 0;
+}
